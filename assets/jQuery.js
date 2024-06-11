c07a36bc1d623e05b671d6429bd6f81b6a956656
{M.Komiya} <{mayu.komiya54@gmail.com}>
2024-06-12 03:46:54 +0900

    ポートフォリオ②作業

--- assets/jQuery.js
+++ assets/jQuery.js
@@ -1,28 +1,20 @@
-// //ham//
-// $(function () {
-//   $(".btn").on("click", function () {
-//     $(".btn").toggleClass("close");
-//     $("nav").fadeToggle(500);
-//   });
-// });
+//hamburger
+$(function () {
+  $(".hamBtn").on("click", function () {
+    $(".hamBtn").toggleClass("close");
+    $("nav").fadeToggle(500);
+  });
+});
 
-// $(function () {
-//   $(".fixBtn").on("click", function () {
-//     $(".fixBtn").toggleClass("close");
-//     $("nav").fadeToggle(500);
-//   });
-
-//   $(window).scroll(function () {
-//     if ($(this).scrollTop() > 150 ) {
-//       $(".fixBtn").fadeIn();
-//     } else {
-//       $(".fixBtn").fadeOut(1);
-//     }
-//   });
-// });
+$(function () {
+  $(".ham_fixBtn").on("click", function () {
+    $(".ham_fixBtn").toggleClass("close");
+    $("nav").fadeToggle(500);
+  });
+});
 
 
-//img next//
+//img next
 const slide = document.getElementById('slide');
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
@@ -64,5 +56,68 @@ indicator.addEventListener('click', (event) => {
   }
 });
 
+//ふわっと表示
+$(function(){
+  $(window).scroll(function (){
+      $('.p-about,.p-goods,.p-store').each(function(){
+          var position = $(this).offset().top;
+          var scroll = $(window).scrollTop();
+          var windowHeight = $(window).height();
+          if (scroll > position - windowHeight + 200){
+            $(this).addClass('active');
+          }
+      });
+  });
+});
+
+//footer list
+$(function(){
+  $('.button').on('click', function(){
+      $(this).parents('.accordion').find('.content').slideToggle(200);
+      $(this).toggleClass('open');
+  });
+});
 
 
+
+//Logo固定
+$(function(){
+  var scrollStart = $('.p-logo').offset().top; //ページ上部からの距離を取得
+  var scrollEnd = $('.p-keyVisual__bottom').offset().top; //ページ上部からの距離を取得
+  var distance = 0;
+
+  $(document).scroll(function(){
+    distance = $(this).scrollTop(); //スクロールした距離を取得
+
+    if (scrollStart <= distance) { //スクロール距離が『.sikaku_box』の位置を超えたら
+      $('.p-logo').addClass('fixed'); //class『fixed』を追加
+    } else if (scrollStart >= distance) { //スクロールがページ上部まで戻ったら
+      $('.p-logo').removeClass('fixed'); //class『fixed』を削除
+    }
+
+    if (scrollEnd <= distance) { //スクロール距離が『.end_box』の位置を超えたら
+      $('.p-logo').addClass('none'); //class『none』を追加
+    } else{
+      $('.p-logo').removeClass('none'); //『.end_box』より上部に戻ったらclass『none』を削除
+    }
+  });
+});
+
+
+//mail
+const $submitBtn = $('#formBtn'); //変数にボタン要素をセット
+$submitBtn.prop('disabled', true); //ボタンは最初はdisabledでクリックできない状態に
+var form = document.querySelector("#form"); //それぞれの入力フォームを変数化
+
+//「もし各入力欄が空だったら」という内容を関数としてセット
+function judge() {
+  if (
+    $(form).val() !== ""
+  ) {
+    $submitBtn.prop('disabled', false); //空じゃなかったらボタンを活性化し、activeクラスをつける
+    $submitBtn.addClass('active');
+  } else {
+    $submitBtn.prop('disabled', true);
+    $submitBtn.removeClass('active');
+  }
+}
